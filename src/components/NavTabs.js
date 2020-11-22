a5e368006bd551d287966cf3283e0fbb90957202
Shalpreet Kaur <kaurshalpreet@gmail.com>
2020-11-22 10:03:17 -0800

    final github deploy

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -29,17 +29,17 @@ function NavTabs() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/">
+              <a className="nav-link" href="#/">
                 About
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/portfolio">
+              <a className="nav-link" href="#/portfolio">
                 Portfolio
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/contact">
+              <a className="nav-link" href="#/contact">
                 Contact
               </a>
             </li>
